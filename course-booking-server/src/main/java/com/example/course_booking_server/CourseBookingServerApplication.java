45c034e5fc8fd571131349502811d6b43474867e
LuongDuy0906 <luognducduy0906@gmail.com>
2026-08-15 19:54:22 +0700

    init course booking personal project

---
+++ course-booking-server/src/main/java/com/example/course_booking_server/CourseBookingServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.course_booking_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseBookingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseBookingServerApplication.class, args);
+	}
+
+}
---
+++ course-booking-server/src/test/java/com/example/course_booking_server/CourseBookingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.course_booking_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseBookingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
